3d8225b66dea32ec2ba1baee7d364c0a09e05cf7
aaminahmq <aaminah.mq@gmail.com>
2024-11-13 20:40:27 -0800

    Delete log_analysis_project directory

--- log_analysis_project/setup_cron_tasks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "0 5 * * * script.sh >> /log_analysis_project/crong_logs/backup.log 2>&1" | crontab -
-
-echo "0 6 * * 1 reminder.sh >> /log_analysis_project/cron_logs/email.log 2>&1" | crontab -
-
-echo "0 0 * * * script.sh >> log_analysis_project/cron_logs/filter_file_size.log 2>&1" | crontab -
-
